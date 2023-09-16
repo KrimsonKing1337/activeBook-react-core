c55b18980e9bacbab9071b2444a2c08fb124f150
Oleg Shilov <shilov-1@yandex.ru>
2023-09-16 17:05:11 +0300

    updated stylelint;

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -14,16 +14,26 @@ module.exports = {
         'ignorePseudoClasses': ['global']
       }
     ],
+    'value-keyword-case': [
+      'lower',
+      {
+        'camelCaseSvgKeywords': true
+      }
+    ],
+
     'scss/dollar-variable-pattern': null,
     'scss/double-slash-comment-whitespace-inside': null,
+    'scss/double-slash-comment-empty-line-before': null,
     'scss/at-import-no-partial-leading-underscore': null,
     'scss/at-rule-no-unknown': true,
 
+    'at-rule-empty-line-before': null,
+    'at-rule-no-unknown': null,
+    'declaration-block-no-redundant-longhand-properties': null,
     'keyframes-name-pattern': null,
     'selector-class-pattern': null,
     'no-descending-specificity': null,
     'declaration-empty-line-before': null,
-    'at-rule-no-unknown': null,
     'selector-pseudo-element-colon-notation': 'single',
     'plugin/declaration-block-no-ignored-properties': true,
 
