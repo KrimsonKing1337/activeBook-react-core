286f06ae4440678f9bf5cf2124a17ebc2475e991
Oleg Shilov <shilov-1@yandex.ru>
2025-03-19 13:34:27 +0300

    added react=fast-compare;

--- lib-esm/src/hooks/effects/background/background.js
+++ lib-esm/src/hooks/effects/background/background.js
@@ -1,14 +1,14 @@
 import { useEffect, useRef } from 'react';
+import isEqual from 'react-fast-compare';
 import { useDispatch } from 'store';
 import { backgroundEffectsActions } from 'store/effects/background';
-import { effectsAreEqual } from './utils';
 export var useBackground = function (effect) {
     var dispatch = useDispatch();
     var refEffect = useRef(null);
     useEffect(function () {
         // если эффект уже есть, и он точно такой же, как тот, что приходит - значит эффект не нужно инициализировать заново
         // todo: сравнивать Component-ы. сейчас сравнивается только на то, есть он или нет
-        if (refEffect.current && effectsAreEqual(refEffect.current, effect)) {
+        if (refEffect.current && isEqual(refEffect.current, effect)) {
             return;
         }
         refEffect.current = effect;
--- src/hooks/effects/background/background.ts
+++ src/hooks/effects/background/background.ts
@@ -1,10 +1,11 @@
 import { useEffect, useRef } from 'react';
 
+import isEqual from 'react-fast-compare';
+
 import { useDispatch } from 'store';
 import { backgroundEffectsActions } from 'store/effects/background';
 
 import type { BackgroundEffect } from './@types';
-import { effectsAreEqual } from './utils';
 
 export const useBackground = (effect: BackgroundEffect) => {
   const dispatch = useDispatch();
@@ -13,8 +14,7 @@ export const useBackground = (effect: BackgroundEffect) => {
 
   useEffect(() => {
     // если эффект уже есть, и он точно такой же, как тот, что приходит - значит эффект не нужно инициализировать заново
-    // todo: сравнивать Component-ы. сейчас сравнивается только на то, есть он или нет
-    if (refEffect.current && effectsAreEqual(refEffect.current, effect)) {
+    if (refEffect.current && isEqual(refEffect.current, effect)) {
       return;
     }
 
