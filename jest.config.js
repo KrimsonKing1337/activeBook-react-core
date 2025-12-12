740e316654b28b5c1efcddcf244d37990272c910
Oleg Shilov <shilov-1@yandex.ru>
2025-12-12 17:54:04 +0300

    updated jest config;

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,18 @@
-module.exports = {
+import packageJson from './package.json' with { type: 'json' };
+
+export default {
   coverageDirectory: '<rootDir>/coverage',
-  preset: 'ts-jest/presets/js-with-babel',
   rootDir: './',
   verbose: true,
   testResultsProcessor: 'jest-sonar-reporter',
+
   globals: {
     __IS_SERVER__: false,
-    __NAME__: require('./package.json').name,
+    __NAME__: packageJson.name,
   },
-  moduleDirectories: [
-    'node_modules',
-    'src'
-  ],
+
+  moduleDirectories: ['node_modules', 'src'],
+
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
     '!src/**/*.d.ts',
@@ -23,39 +24,52 @@ module.exports = {
     '!src/**/__test__/**/*',
     '!src/**/mixins/**/*',
     '!src/**/index.ts',
-    '!src/**/index.ts',
     '!src/**/styles.ts',
     '!src/**/*.stories.*',
     '!**/node_modules/**',
   ],
-  coveragePathIgnorePatterns: [],
-  setupFiles: [
-    'react-app-polyfill/jsdom'
+
+  coveragePathIgnorePatterns: [
+    'manifest.ts',
+    'example.tsx',
+    'analytics-utils.ts',
+    'use-isomorphic-layout-effect.ts',
+    'api.ts',
+    'slice.ts',
+    'saga.ts',
+    'memo.tsx',
+    'with-helmet.tsx',
   ],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/test/setup-tests.ts'
-  ],
+
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup-tests.ts'],
+
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
   ],
   testEnvironment: 'jsdom',
+
   transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babel-transform.js',
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': ['babel-jest', { configFile: './config/jest/babel.config.js' }],
     '^.+\\.css$': '<rootDir>/config/jest/css-transform.js',
-    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/file-transform.js'
+    '^(?!.*\\.(t|j)sx?|css|json)$': '<rootDir>/config/jest/file-transform.js',
   },
+
   transformIgnorePatterns: [],
-  modulePaths: [],
+
   moduleNameMapper: {
     '.+\\.(css|scss|png|jpg|jpeg|ttf|woff|woff2)$': 'jest-transform-stub',
     '\\.svg': '<rootDir>/src/test/svg-mock.ts',
     '^react($|/.+)': '<rootDir>/node_modules/react$1',
   },
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
   watchPlugins: [
     'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
+    'jest-watch-typeahead/testname',
   ],
-  resetMocks: true
+
+  resetMocks: true,
 };
